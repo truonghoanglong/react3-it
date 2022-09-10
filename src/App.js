250e0da6988f0c6b67c0c6a74ac4ed2254974719
truonghoanglong <57944514+truonghoanglong@users.noreply.github.com>
2022-09-10 11:59:39 +0700

    109

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import TodoList from "./components/TodoList";
 function App() {
 	return (
 		<div className="App">
-			Hello
 			<TodoList/>
 		</div>
 	);
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TodoList = () => {
+    const [state,setState] = useState('')
+    const handleSubmit = (e,msg) => {
+        console.log("🚀 ~ file: TodoList.jsx ~ line 5 ~ TodoList ~ state", state)
+        
+    }
     return (
-        <div>TodoList</div>
+        <div>
+            <input type="text" value={state} onChange={(e)=>setState(e.target.value)}/>
+            <button type="button" onClick={ (e) => handleSubmit(e,'hoanglong') }>SUBMIT</button>
+        </div>
     )
 }
 
